7df76be6aa41be098201227328b008119e76e363
Hinkers <24424803+hinkers@users.noreply.github.com>
2026-01-18 02:17:56 +0000

    Added rplay command

--- cogs/audio.py
+++ cogs/audio.py
@@ -190,7 +190,20 @@ class AudioCog(commands.Cog, name='Audio'):
                 await ctx.send(f'Favourite not found.')
                 return
             song = favourite.song
-            
+
+            await self.do_play(ctx, song, session)
+
+    @commands.hybrid_command()
+    async def rplay(self, ctx: commands.context):
+        """ Play a random song from the database. """
+        await ctx.typing()
+
+        async with self.bot.session as session:
+            song = await Song.get_random(session)
+            if song is None:
+                await ctx.send('No downloaded songs available in the database.')
+                return
+
             await self.do_play(ctx, song, session)
 
     @commands.hybrid_command()
@@ -387,6 +400,7 @@ class AudioCog(commands.Cog, name='Audio'):
     @connect.before_invoke
     @play.before_invoke
     @playf.before_invoke
+    @rplay.before_invoke
     @playlist.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
